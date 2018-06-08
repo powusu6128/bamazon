110266930a9fb5c8e1e364d895beac23b7f608ee
Philip Owusu <philipowusu@Philips-MacBook-Pro.local>
2018-06-08 03:18:36 -0500

    adding again

---
+++ bamazon2sql.sql
@@ -0,0 +1,75 @@
+create database if not exists bamazon2;
+
+show databases;
+
+USE bamazon2;
+
+create table products (
+  item_id integer  not null auto_increment primary key,
+
+  product_name varchar(40) NOT NULL,
+
+  department_name varchar (40) NULL,
+
+  price decimal(10,2) not null,
+
+  stock_quantity int not null default '0'
+);
+
+Select * From products;
+
+insert into products(product_name, department_name,price, stock_quantity)
+
+values('shoes','dsw',12,120),
+
+('phone','electronics',560,25),
+
+('banana','walmar',1.99,60),
+
+('paint','ware house',5,50),
+
+('paris bow','kitch appliance',12,60),
+
+('shoes','dsw',12,60),
+ 
+('home theater','electronic',1200,5),
+
+('tomatoe sidling','gardening',0.99,360),
+
+('door','ware house',120,25),
+
+('table','slamberland',240,5),
+
+ ('fridge','sears',1200,25);
+
+
+
+CREATE TABLE departments (
+  department_id  int not null auto_increment primary key,
+
+  department_name varchar (100) NOT NULL,
+
+  overhead_costs decimal (10,2) NOT NULL default '0.00'
+
+);
+
+select * from departments;
+
+
+insert into departments(department_name,overhead_costs)
+values('dsw', 2000),
+			('electronics', 1500),
+            ('gardening', 1000),
+            ('slanberland', 7000),
+            ('walmart', 2000);
+            
+            
+alter table products add column product_sales decimal  default 00.00;
+
+select departments.department_id,departments.department_name,departments.overhead_costs,products.product_sales,SUM(products.product_sales - departments.overhead_costs) as total_sales
+from departments
+inner join products on
+departments.department_name = products.department_name
+group by departments.department_name,departments.overhead_costs,products.product_sales ,departments.department_id
+ORDER BY total_sales DESC limit 10;
+
---
+++ bamazonManager.js
@@ -0,0 +1,290 @@
+
+var mysql = require('mysql');
+var prompt = require('prompt');
+var inquirer = require('inquirer');
+var Table = require('cli-table');
+
+// connect to the required database 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'bamazon2',
+    socketPath: '/tmp/mysql.sock'
+});
+
+var table = new Table({
+    chars: {
+        'top': '═',
+        'top-mid': '╤',
+        'top-left': '╔',
+        'top-right': '╗',
+        'bottom': '═',
+        'bottom-mid': '╧',
+        'bottom-left': '╚',
+        'bottom-right': '╝',
+        'left': '║',
+        'left-mid': '╟',
+        'mid': '─',
+        'mid-mid': '┼',
+        'right': '║',
+        'right-mid': '╢',
+        'middle': '│'
+    },
+    head: ['Item_ID', 'Product Name', 'Department Name','Price','Stock Products','Product Sales']
+
+});
+
+promptManagerAction();
+
+// promptManagerAction will present menu options to the manager and trigger appropriate logic
+
+function promptManagerAction() {
+
+	// Prompt the manager to select an option
+	inquirer.prompt([
+		{
+			type: 'list',
+			name: 'option',
+			message: 'Please select an option:',
+			choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
+			filter: function (val) {
+				if (val === 'View Products for Sale') {
+					return 'sale';
+				} else if (val === 'View Low Inventory') {
+					return 'lowInventory';
+				} else if (val === 'Add to Inventory') {
+					return 'addInventory';
+				} else if (val === 'Add New Product') {
+					return 'newProduct';
+				} else {
+					// This case should be unreachable
+					console.log('ERROR: Unsupported operation!');
+					exit(0);
+				}
+			}
+		}
+	]).then(function(input) {
+		
+		// Trigger the appropriate action based on the user input
+		if (input.option ==='sale') {
+			display_products();
+		} else if (input.option === 'lowInventory') {
+			display_LowInventory();
+		} else if (input.option === 'addInventory') {
+			add_Inventory();
+		} else if (input.option === 'newProduct') {
+			create_NewProduct();
+		} else {
+			// This case should be unreachable
+			console.log('ERROR: Unsupported operation!');
+			exit(0);
+		}
+	})
+}
+
+
+// validateInteger makes sure that the user is supplying only positive integers for their inputs
+function validateInteger(value) {
+	var integer = Number.isInteger(parseFloat(value));
+	var sign = Math.sign(value);
+
+	if (integer && (sign === 1)) {
+		return true;
+	} else {
+		return 'Please enter a whole non-zero number.';
+	}
+}
+
+// validateNumeric makes sure that the user is supplying only positive numbers for their inputs
+function validateNumeric(value) {
+	// Value must be a positive number
+	var number = (typeof parseFloat(value)) === 'number';
+	var positive = parseFloat(value) > 0;
+
+	if (number && positive) {
+		return true;
+	} else {
+		return 'Please enter a positive number for the unit price.'
+	}
+}
+
+// display all the products available. 
+
+function display_products() {
+	
+	// Construct the db query string
+	queryStr = 'SELECT * FROM products';
+
+	// Make the db query
+	connection.query(queryStr, function(err, data) {
+		if (err) throw err;
+
+        console.log('.........................\n');
+		console.log('*** Existing Inventory *** ');
+		console.log('.........................\n');
+
+		for (var i = 0; i < data.length; i++) {
+			table.push([data[i].item_id,data[i].product_name,data[i].department_name,data[i].price,data[i].stock_quantity,data[i].product_sales])
+		}
+          console.log(table.toString());
+          console.log("---------------------------------------------------------------------\n");
+          connection.end();
+	  	
+	})
+}
+
+// Display Low Inventory . 
+function display_LowInventory() {
+	
+	// Construct the db query string
+	queryStr = 'SELECT * FROM products WHERE stock_quantity < 5';
+
+	// Make the db query
+	connection.query(queryStr, function(err, data) {
+		
+		if (err) throw err;
+
+		if(! data[0].stock_quantity <5) {
+			connection.end();
+			
+			return console.log("Sorry,All stocks are up to capacity!")
+		}
+		else{
+
+			for (var i = 0; i < data.length; i++) {
+            
+				table.push([data[i].item_id,data[i].product_name,data[i].department_name,data[i].price,data[i].stock_quantity,data[i].product_sales]);
+	
+			}
+		}
+		
+        console.log('.......................................\n');
+		console.log('*** Low Inventory Items (below 100): *** ');
+		console.log('.......................................\n');
+
+		
+        
+        console.log(table.toString());
+
+	  	console.log("---------------------------------------------------------------------\n");
+
+		// End the database connection
+		connection.end();
+	})
+}
+
+//add inventory . 
+
+function add_Inventory() {
+	
+	// Prompt the user to select an item
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'item_id',
+			message: 'Please enter the Item ID for stock_count update.',
+			validate: validateInteger,
+			filter: Number
+		},
+		{
+			type: 'input',
+			name: 'quantity',
+			message: 'How many would you like to add?',
+			validate: validateInteger,
+			filter: Number
+		}
+	]).then(function(input) {
+		// console.log('Manager has selected: \n    item_id = '  + input.item_id + '\n    additional quantity = ' + input.quantity);
+
+		var item = input.item_id;
+		var addQuantity = input.quantity;
+
+		// Query db to confirm that the given item ID exists and to determine the current stock_count
+		var queryStr = 'SELECT * FROM products WHERE ?';
+
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+			if (err) throw err;
+
+			// If the user has selected an invalid item ID, data attay will be empty
+
+			if (data.length === 0) {
+				console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+
+				addInventory();
+
+			} else {
+				var productData = data[0];
+
+				console.log('Updating Inventory...');
+
+				// Construct the updating query string
+				var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity + addQuantity) + ' WHERE item_id = ' + item;
+
+				// Update the inventory
+				connection.query(updateQueryStr, function(err, data) {
+					if (err) throw err;
+
+					console.log('Stock count for Item ID ' + item + ' has been updated to ' + (productData.stock_quantity + addQuantity) + '.');
+
+					console.log("\n---------------------------------------------------------------------\n");
+
+					// End the database connection
+					connection.end();
+				})
+			}
+		})
+	})
+}
+
+// createNewProduct will guide the user in adding a new product to the inventory
+function create_NewProduct() {
+
+	// Prompt the user to enter information about the new product
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'product_name',
+			message: 'Please enter the new product name.',
+		},
+		{
+			type: 'input',
+			name: 'department_name',
+			message: 'Department name the new product belong to?',
+		},
+		{
+			type: 'input',
+			name: 'price',
+			message: 'What is the price per unit?',
+			validate: validateNumeric
+		},
+		{
+			type: 'input',
+			name: 'stock_quantity',
+			message: 'How many items are in stock?',
+			validate: validateInteger
+		}
+	]).then(function(input) {
+		// console.log('input: ' + JSON.stringify(input));
+
+		console.log('Adding New Item: \n    product_name = ' + input.product_name + '\n' +  
+									   '    department_name = ' + input.department_name + '\n' +  
+									   '    price = ' + input.price + '\n' +  
+									   '    stock_quantity = ' + input.stock_quantity);
+
+		// Create the insertion query string
+		var queryStr = 'INSERT INTO products SET ?';
+
+		// Add new product to the db
+		connection.query(queryStr, input, function (error, results, fields) {
+			if (error) throw error;
+
+			console.log('New product has been added to the inventory under Item ID ' + results.insertId + '.');
+			console.log("\n---------------------------------------------------------------------\n");
+
+			// End the database connection
+			connection.end();
+		});
+	})
+}
